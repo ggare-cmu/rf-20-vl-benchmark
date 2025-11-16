335a76d647b18cdaecabb76672e59f4067cc3b2e
Gautam R Gare <ggare@andrew.cmu.edu>
2025-11-16 00:24:38 -0500

    run file

--- ipt/run_seq_all_ipt.sh
+++ ipt/run_seq_all_ipt.sh
@@ -5,19 +5,19 @@ set -e  # Exit if any command fails
 WORKDIR=$(pwd)
 
 
-MODEL_NAME="Qwen2.5-VL-7B-Instruct" #Qwen2.5-VL-7B-Instruct, Qwen2.5-VL-72B-Instruct, Qwen3-VL-30B-A3B-Instruct, Qwen3-VL-235B-A22B-Instruct
+# MODEL_NAME="Qwen2.5-VL-7B-Instruct" #Qwen2.5-VL-7B-Instruct, Qwen2.5-VL-72B-Instruct, Qwen3-VL-30B-A3B-Instruct, Qwen3-VL-235B-A22B-Instruct
 # MODEL_NAME="Qwen2.5-VL-72B-Instruct"
 # MODEL_NAME="Qwen3-VL-8B-Instruct"
 # MODEL_NAME="Qwen3-VL-30B-A3B-Instruct"
 # MODEL_NAME="Qwen3-VL-235B-A22B-Instruct"
 # MODEL_NAME="Qwen3-VL-235B-A22B-Instruct-FP8"
-
+MODEL_NAME="gemini-2.5-pro-preview-03-25"
 
 # CMD_TEMPLATE="python ipt/run_bench_singleclass_IPT.py --model_name gemini-2.5-pro-preview-03-25 --ipt_mode --num_ipt_iterations 10 --output_dir results/rf100vl_IPT_Gemini/gemini-2.5-pro-preview-03-25/rf20_IPT_singleclass_Novqa --vqa_batch_size 1"
 # CMD_TEMPLATE="python ipt/run_bench_singleclass_IPT.py --model_name gemini-2.5-pro-preview-03-25 --ipt_mode --vqa_rescore --apply_nms --nms_threshold 0.5 --num_ipt_iterations 10 --output_dir results/rf100vl_IPT_Gemini/gemini-2.5-pro-preview-03-25/rf20_IPT_singleclass_vqaScore_withNMS --vqa_batch_size 1"
 # CMD_TEMPLATE="python ipt/run_bench_singleclass_IPT.py --model_name Qwen3-VL-235B-A22B-Instruct-FP8 --ipt_mode --vqa_rescore --apply_nms --nms_threshold 0.5 --num_ipt_iterations 10 --output_dir results/rf100vl_IPT/Qwen3-VL-235B-A22B-Instruct-FP8/rf20_IPT_singleclass_vqaScore_withNMS --vqa_batch_size 1"
-RESULTSDIR="results/rf100vl_IPT/$MODEL_NAME/rf20_IPT_singleclass_vqaScore_withNMS"
-CMD_TEMPLATE="python ipt/run_bench_singleclass_IPT.py --model_name $MODEL_NAME --ipt_mode --vqa_rescore --apply_nms --nms_threshold 0.5 --num_ipt_iterations 10 --output_dir $RESULTSDIR --vqa_batch_size 1"
+RESULTSDIR="results/rf100vl_IPT_Gemini/$MODEL_NAME/rf20_IPT_singleclass_Novqa"
+CMD_TEMPLATE="python ipt/run_bench_singleclass_IPT.py --model_name $MODEL_NAME --ipt_mode --num_ipt_iterations 10 --output_dir $RESULTSDIR --vqa_batch_size 1"
 # CMD_TEMPLATE="source /home/ubuntu/miniforge3/etc/profile.d/conda.sh && conda activate qwen-vllm-env && python ipt/run_bench_singleclass_IPT.py --model_name $MODEL_NAME --ipt_mode --vqa_rescore --apply_nms --nms_threshold 0.5 --num_ipt_iterations 10 --output_dir $RESULTSDIR --vqa_batch_size 1"
 
 
